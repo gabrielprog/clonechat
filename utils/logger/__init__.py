b1030871a8d02ee2ceb71313fd545ba1a7887062
Gabriel <gabrielprogramado@gmail.com>
2024-08-28 20:28:43 -0300

    feat: Adicionando entry-point exception handling e escrevendo log deles

---
+++ utils/logger/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+FILENAME_LOG = "exceptions.log"
+logging.basicConfig(
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename=FILENAME_LOG,
+    filemode="a"
+)
+
+def debug(message):
+    logging.debug(message)
+
+def info(message):
+    logging.info(message)
+
+def warning(message):
+    logging.warning(message)
+
+def error(message):
+    logging.error(message)
+
+def critical(message):
+    logging.critical(message)
